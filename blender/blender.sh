8babae2bfaa4905655a8d6e4d695c8894d741635
owoin <info@owoin.com>
2018-08-25 14:27:05 +0800

    add an

--- blender/blender.sh
+++
@@ -1,3 +0,0 @@
-ln -sf /opt/git/i18n/blender/blender-2.78.mo /opt/blender/blender-2.78/2.78/datafiles/locale/zh_CN/LC_MESSAGES/blender.mo
-ln -sf /opt/git/i18n/blender/blender-2.79.mo /opt/blender/blender-2.79/2.79/datafiles/locale/zh_CN/LC_MESSAGES/blender.mo
-ln -sf /opt/git/i18n/blender/blender-2.80.mo /opt/blender/blender-2.80/2.80/datafiles/locale/zh_CN/LC_MESSAGES/blender.mo
--- blender/msgunfmt.sh
+++
@@ -1,3 +0,0 @@
-msgunfmt blender-2.78.mo -o blender-2.78.po
-msgunfmt blender-2.79.mo -o blender-2.79.po
-msgunfmt blender-2.80.mo -o blender-2.80.po
